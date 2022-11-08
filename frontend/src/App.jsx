e8a9a5b776012471a95eb8b7bc2fded582293bb7
João Chaves <jomp@pop-os.localdomain>
2022-11-08 09:28:27 -0300

    feat: Configurando rota da página de cadastro de usuário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import UserRegister from './pages/UserRegister/UserRegister';
 
 function App() {
 
 return (
-	<>
-	</>
+	<BrowserRouter>
+		<Routes>
+			<Route path='/cadastro' element={<UserRegister />} />
+		</Routes>
+	</BrowserRouter>
 )}
 
 export default App
