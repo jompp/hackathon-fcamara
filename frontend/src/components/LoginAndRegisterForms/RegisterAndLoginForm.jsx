bc273bc7cd2a6efccc6d3b512f3e6d25a15f9ab3
João Chaves <jomp@pop-os.localdomain>
2022-11-08 17:52:10 -0300

    feat: Adicionando logo e slogan no form

--- frontend/src/components/LoginAndRegisterForms/RegisterAndLoginForm.jsx
+++ frontend/src/components/LoginAndRegisterForms/RegisterAndLoginForm.jsx
@@ -7,46 +7,54 @@ import lockIcon from '../../assets/icons/lock.svg'
 import eyeSlashIcon from '../../assets/icons/eye-slash.svg'
 import eyeIcon from '../../assets/icons/eye.svg'
 import arrowLeftIcon from '../../assets/icons/arrow-left.svg'
+import logo from '../../assets/images/logo.png'
 
 export default function Form(props) {
   return (
-    <form className='login-register-form'>
-			<legend>Crie sua conta</legend>
-			
-			<fieldset>
-				<div className="ipt-err-box">
-					<div className="icon-ipt-box">
-						<img className='icon' src={userIcon} alt="Ícone de usuário" />
-						<input type="text" name="name" placeholder='Seu nome'/>
-					</div>
-					<span>alguma coisa</span>
-				</div>
-
-				<div className="ipt-err-box">
-					<div className="icon-ipt-box">
-						<img className='icon' src={emailIcon} alt="Ícone de email" />
-						<input type="text" name="email" placeholder='Seu email'/>
-					</div>
-					<span>alguma coisa</span>
-				</div>
-
-				<div className="ipt-err-box">
-					<div className="icon-ipt-box">
-						<img className='icon' src={lockIcon} alt="Ícone de cadeado" />
-						<input type="password" name="password" placeholder='Sua senha'/>
-						<img className='icon eye-slash' src={eyeSlashIcon} alt="Ícone de cadeado" />
-					</div>
-					<span>alguma coisa</span>
-				</div>
-			</fieldset>
-
-			{props.text}
-			<button type='submit'>{props.textButton}</button>
-			<div className='back-to-login-box'>
-				<img src={arrowLeftIcon} alt="Seta para a esquerda" />
-				<a href="#">Voltar para o login</a>
+		<section className='form-logo-box'>
+			<div className='slogan-logo-box'>
+				<img src={logo} alt="Logo" />
+				<h2>A plataforma mais vitaminada do mundo</h2>
 			</div>
-    </form>
+
+			<form className='login-register-form'>
+				<legend>Crie sua conta</legend>
+				
+				<fieldset>
+					<div className="ipt-err-box">
+						<div className="icon-ipt-box">
+							<img className='icon' src={userIcon} alt="Ícone de usuário" />
+							<input type="text" name="name" placeholder='Seu nome'/>
+						</div>
+						<span>alguma coisa</span>
+					</div>
+
+					<div className="ipt-err-box">
+						<div className="icon-ipt-box">
+							<img className='icon' src={emailIcon} alt="Ícone de email" />
+							<input type="text" name="email" placeholder='Seu email'/>
+						</div>
+						<span>alguma coisa</span>
+					</div>
+
+					<div className="ipt-err-box">
+						<div className="icon-ipt-box">
+							<img className='icon' src={lockIcon} alt="Ícone de cadeado" />
+							<input type="password" name="password" placeholder='Sua senha'/>
+							<img className='icon eye-slash' src={eyeSlashIcon} alt="Ícone de cadeado" />
+						</div>
+						<span>alguma coisa</span>
+					</div>
+				</fieldset>
+
+				{props.text}
+				<button type='submit'>{props.textButton}</button>
+				<div className='back-to-login-box'>
+					<img src={arrowLeftIcon} alt="Seta para a esquerda" />
+					<a href="#">Voltar para o login</a>
+				</div>
+			</form>
+		</section>
   )
 }
 
--- frontend/src/pages/UserRegister/UserRegister.jsx
+++ frontend/src/pages/UserRegister/UserRegister.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import './UserRegister.css'
+
 import Form from '../../components/LoginAndRegisterForms/RegisterAndLoginForm'
 
 export default function UserRegister() {
   return (
-    <>
+    <div className='register-form-page'>
       <Form 
         text={<p className='form-text'>Ao se registrar, você aceita nossos <a href='#'>termos de uso</a> e a nossa <a href="#">política de privacidade</a>.</p>}
         textButton='Cadastrar'
       />
-    </>
+    </div>
   )
 }
