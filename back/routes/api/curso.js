f03695c799649dc0e04d6ea015d88026b2a5a40f
BeahIF <beatrizdacosta1@Gmail.com>
2022-11-13 17:39:58 -0300

    feat:get by id de curso

--- back/routes/api/curso.js
+++ back/routes/api/curso.js
@@ -3,6 +3,8 @@ const router= express.Router();
 const cursoController = require('../../controllers/cursoController')
 const auth = require("../../middleware/auth")
 router.post("/",cursoController.create)
-router.post("/addTrilha", auth.decodeToken, cursoController.addTrilha)
+router.post("/addTrilha",  cursoController.addTrilha)
 router.get("/", cursoController.getAll)
+router.get("/:id", cursoController.getById)
+
 module.exports = router;
