43490b64672616e0402ed7aeb57256d82f02eb7c
João Chaves <jomp@pop-os.localdomain>
2022-11-13 23:04:59 -0300

    feat: Criando a seção dos cursos não iniciados

--- frontend/src/pages/UserCursosHomepage/UserCursos.jsx
+++ frontend/src/pages/UserCursosHomepage/UserCursos.jsx
@@ -4,7 +4,9 @@ import './UserCursos.css'
 import LoggedUserNavbar from '../../components/LoggedUserNavbar/LoggedUserNavbar'
 import CourseCard from '../../components/CourseCard/CourseCard'
 
-import CourseImg from '../../assets/images/fs-course.jpg'
+import FsCourseImg from '../../assets/images/fs-course.jpg'
+import QaCourseImg from '../../assets/images/qa-course.jpg'
+import UxCourseImg from '../../assets/images/ux-course.jpg'
 
 export default function UserCursos() {
   return (
@@ -14,7 +16,7 @@ export default function UserCursos() {
         <section className='continue-the-course'>
             <h2>Continue de onde parou</h2>
             <CourseCard 
-                courseImg={CourseImg}
+                courseImg={FsCourseImg}
                 courseTitle='Full Stack Developer'
                 courseInfo={
                     <p>Linguagem de programação;<br/>Lógica de programação e algoritmos;<br/>HTML, CSS e Bootstrap;<br/>HTTP e HTTPS;<br/>E muito mais sobre o universo tech!</p>
@@ -22,6 +24,30 @@ export default function UserCursos() {
                 completionPercentage='30% concluído'
                 lastAccess='Útimo acesso em 07/11/2022'
                 btnTxt='Acessar'
+            />
+        </section>
+        <section className='available-courses'>
+            <h2>Trilhas disponíveis</h2>
+            <h3>Para se tornar QA Tester</h3>
+            <CourseCard 
+                courseImg={QaCourseImg}
+                courseTitle='Quality Assurence'
+                courseInfo={
+                    <p>Bancos de dados;<br/>Automação de testes;<br/>Qualidade de software;<br/>Github;<br/>Aproveite e veja a vida sem bugs depois!</p>
+                }
+                completionPercentage='0% concluído'
+                btnTxt='Iniciar trilha'
+            />
+
+            <h3>Para se tornar Product Designer</h3>
+            <CourseCard 
+                courseImg={UxCourseImg}
+                courseTitle='UI/UX Design'
+                courseInfo={
+                    <p>Teoria das cores;<br/>Protótipo para testes;<br/>Avaliação comparativa;<br/>Como realizar pesquisas;<br/>E muito mais sobre designer e usuários!</p>
+                }
+                completionPercentage='0% concluído'
+                btnTxt='Iniciar trilha'
             />
         </section>
     </main>
