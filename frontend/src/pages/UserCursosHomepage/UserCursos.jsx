7c930edf92c8d7f7d228ab28aed55846d305fd93
João Chaves <jomp@pop-os.localdomain>
2022-11-13 21:42:40 -0300

    feat: Configurando rota e criando componente da página home de cursos

---
+++ frontend/src/pages/UserCursosHomepage/UserCursos.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './UserCursos.css'
+
+import LoggedUserNavbar from '../../components/LoggedUserNavbar/LoggedUserNavbar'
+
+export default function UserCursos() {
+  return (
+    <>
+    <LoggedUserNavbar />
+    <main>
+        
+    </main>
+    </>
+  )
+}
