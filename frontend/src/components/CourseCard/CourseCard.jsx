a27b695966a5d128c04463641e69896857c96ff1
João Chaves <jomp@pop-os.localdomain>
2022-11-14 11:04:23 -0300

    Fazendo ajustes na responsividade do card de curso

--- frontend/src/components/CourseCard/CourseCard.jsx
+++ frontend/src/components/CourseCard/CourseCard.jsx
@@ -7,6 +7,7 @@ export default function CourseCard(props) {
   return (
     <div className='course-card'>
         <div className='img-info-box'>
+            <h4 className='course-title-mobile'>{props.courseTitle}</h4>
             <img src={props.courseImg} alt="Imagem representativa do curso" />
             <div className='title-info-box'>
                 <h4>{props.courseTitle}</h4>
