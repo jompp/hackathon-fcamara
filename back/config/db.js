040686c23f85c122a2e0f3bd7c6eb312c4ac5ee2
BeahIF <beatrizdacosta1@Gmail.com>
2022-11-15 17:26:19 -0300

    ajuste de header no swagger

--- back/config/db.js
+++ back/config/db.js
@@ -1,16 +1,11 @@
 require('dotenv/config')
 
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-=======
 const config = require('config');
-//const db = config.get('mongoURI');
->>>>>>> db3f54e9e01528de095289b9931f489743301db6
 
 const connectDB= async ()=>{
-    console.log(process.env.DB_URI)
     try{
-        await mongoose.connect(process.env.DB_URI,{
+        await mongoose.connect(process.env.mongoURI,{
             useNewUrlParser: true
         })
     console.log('MongoDB Connected...')
