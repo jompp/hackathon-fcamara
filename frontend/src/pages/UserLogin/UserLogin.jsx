08548fb32f0037315511b8010177e9fb7e03d8c2
João Chaves <jomp@pop-os.localdomain>
2022-11-15 12:37:48 -0300

    feat: Lidando com erros na requisição de login

--- frontend/src/pages/UserLogin/UserLogin.jsx
+++ frontend/src/pages/UserLogin/UserLogin.jsx
@@ -1,19 +1,33 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './UserLogin.css'
 
 import * as yup from "yup";
 import api from '../../service/api'
 
 import Form from '../../components/LoginAndRegisterForms/RegisterAndLoginForm'
+import LoadingCircle from '../../components/LoadingCircle/LoadingCircle';
 
 export default function UserLogin() {
+  const [waitingResponse, setWaitingResponse] = useState(false)
   const [areCredencialsWrong, setAreCredencialsWrong] = useState(false)
+  const navigate = useNavigate()
 
-  const sendForm = async userData => { 
+  const sendForm = async userData => {
+    setWaitingResponse(true) 
     delete userData.name 
-    console.log(userData) 
-    const response = await api.post('/api/auth', userData)
-    console.log(response)
+    try {
+      const response = await api.post('/api/auth', userData)
+      console.log(response)
+      navigate('/cursos')
+    } catch(e) {
+      if(e.response.status === 400) {
+        setAreCredencialsWrong(true)
+      } else {
+        alert("Ops, alguma coisa deu errado. Tente novamente mais tarde.")
+      }
+    }
+    setWaitingResponse(false)
   }
 	
 	return (
@@ -21,7 +35,7 @@ export default function UserLogin() {
 			<Form
 				formLegend='Faça seu login'
 				text={<p className='form-text'>Não tem conta? <a href='/cadastro'>Crie sua conta.</a></p>}
-        textButton='Entrar'
+        textButton={waitingResponse ? <LoadingCircle /> : 'Entrar'}
         yupSchema={yup.object({
           email: yup.string().required('Preencha esse campo'),
           password: yup.string().required('Preencha esse campo')
