cea577eb1fe1d381f5d22a2d3c24096cfde8b5a7
BeahIF <beatrizdacosta1@Gmail.com>
2022-11-15 17:32:59 -0300

    ajustando variavel de ambiente

--- back/config/db.js
+++ back/config/db.js
@@ -5,7 +5,7 @@ const config = require('config');
 
 const connectDB= async ()=>{
     try{
-        await mongoose.connect(process.env.mongoURI,{
+        await mongoose.connect(process.env.DB_URI,{
             useNewUrlParser: true
         })
     console.log('MongoDB Connected...')
